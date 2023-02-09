05d49cde7209de011d13cfb598f39aff30979dd2
Paula Bewick <116111867+PaulaBewick@users.noreply.github.com>
2023-02-08 18:03:09 -0600

    changes and updates to portfolio site, adding api test

--- public/apitest.js
+++ public/apitest.js
@@ -1,22 +1,125 @@
-// apitest.html js
+// Function to generate random year
 
-// Constant URL value for JAAS API
-const API_URL = "https://fakerapi.it/api/v1/";
+// Function to generate random year
 
-const apiForm = document.querySelector("#api-form");
-// console.log(musicForm);
-apiForm.addEventListener("submit", getSpacePhoto);
-console.log("loaded");
-function getSpacePhoto(event) {
-  event.preventDefault();
-  console.log("here");
-  let formData = new FormData(apiForm);
-  console.log(formData);
-  fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=gM26T2mHGZnfJauqVf3DIGnVp1N327ULC23ChufV`
-  ).then((response) => {
-    console.log(response);
-  });
+function getRandomYear() {
+  return Math.floor(Math.random() * (2022 - 1996 + 1)) + 1996;
 }
 
-// https://fakerapi.it/api/v1/images?_quantity=1&_type=kittens&_height=300
+const randomYear = getRandomYear();
+
+var month = document.getElementById("month");
+var day = document.getElementById("day");
+
+const months = {
+  January: "01",
+  February: "02",
+  March: "03",
+  April: "04",
+  May: "05",
+  June: "06",
+  July: "07",
+  August: "08",
+  September: "09",
+  October: "10",
+  November: "11",
+  December: "12",
+};
+
+let currentBirthday = "";
+
+function updateBirthday() {
+  let yourMonth = months[month.options[month.selectedIndex].value];
+  let yourDay = ("0" + day.options[day.selectedIndex].value).slice(-2);
+  // fix this so that the checker for undefined works
+  let birthday = `${randomYear}-${yourMonth}-${yourDay}`;
+  return birthday;
+}
+
+function checkBirthday() {
+  if (!month.value || !day.value) {
+    return;
+  }
+  let updatedBirthday = updateBirthday();
+  if (currentBirthday !== updatedBirthday) {
+    currentBirthday = updatedBirthday;
+    getSpacePhoto();
+  }
+}
+
+month.addEventListener("change", (e) => {
+  e.preventDefault();
+  checkBirthday();
+});
+
+day.addEventListener("change", (e) => {
+  e.preventDefault();
+  checkBirthday();
+});
+
+// console.log(updateBirthday());
+
+const nasaUrl = "https://api.nasa.gov/planetary/apod?api_key=";
+const apiKey = "gM26T2mHGZnfJauqVf3DIGnVp1N327ULC23ChufV";
+const request = new XMLHttpRequest();
+request.open("GET", nasaUrl + apiKey);
+request.send();
+
+function getSpacePhoto() {
+  const title = document.querySelector("#title");
+  const copyright = document.querySelector("#copyright");
+  const mediaSection = document.querySelector("#media-section");
+  const information = document.querySelector("#description");
+
+  const imageSection = `<a id="hdimg" href="" target="-blank">
+  <div class="image-div">
+  <img id="image_of_the_day" src="" alt="image-by-nasa">
+  </div>
+ </a>`;
+
+  const videoSection = `<div class="video-div"> <iframe id="videoLink" src="" frameborder="0"></iframe></div>`;
+
+  let newDate = "&date=" + updateBirthday() + "&";
+
+  function fetchData() {
+    try {
+      fetch(nasaUrl + apiKey + newDate)
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          diplaydata(json);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function diplaydata(data) {
+    title.innerHTML = data.title;
+
+    if (data.hasOwnProperty("copyright")) {
+      copyright.innerHTML = data.copyright;
+    } else {
+      copyright.innerHTML = "";
+    }
+
+    date.innerHTML = data.date;
+
+    if (data.media_type == "video") {
+      mediaSection.innerHTML = videoSection;
+      document.getElementById("videoLink").src = data.url;
+    } else {
+      mediaSection.innerHTML = imageSection;
+      document.getElementById("hdimg").href = data.hdurl;
+      document.getElementById("image_of_the_day").src = data.url;
+    }
+    information.innerHTML = data.explanation;
+  }
+  fetchData();
+}
+
+// request.addEventListener("load", function () {
+//   if (request.status == 200 && request.readyState == 4) {
+//     getSpacePhoto();
+//   }
+// });
--- public/css/css-variables/theme/theme.js
+++ public/css/css-variables/theme/theme.js
@@ -3,6 +3,13 @@ const toggleButton = document.querySelector(".toggle-button");
 toggleButton.addEventListener("change", toggleTheme, false);
 
 const theme = {
+  system: {
+    "--primary-color": "#004f6b",
+    "--secondary-color": "#1a8199",
+    "--font-color": "#1a1a1a",
+    "--bg-color": "#e9e7ea",
+    "--heading-color": "#1a8199",
+  },
   light: {
     "--primary-color": "#004f6b",
     "--secondary-color": "#1a8199",
@@ -56,7 +63,30 @@ const preferredTheme = localStorage.getItem("theme");
 if (preferredTheme === "dark") {
   useTheme("dark");
   toggleButton.checked = true;
-} else {
+} else if (preferredTheme === "light") {
   useTheme("light");
   toggleButton.checked = false;
+} else {
+  useTheme("system");
+  // toggleButton.checked = false;
 }
+
+// scrolling animations
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
